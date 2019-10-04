c978725e8676651670c55db058710679e74205c7
ty tran <tytv1@fsoft.com>
2019-10-04 16:21:41 +0700

    Initial commit

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BeenTogether
+//
+//  Created by Trần Tý on 10/4/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: NSObject, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = DashboardViewController()
+    self.window?.makeKeyAndVisible()
+    return true
+  }
+}
