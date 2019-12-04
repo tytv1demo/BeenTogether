1943a565527cef6b3ca88e020fd318230823c0fe
Quan Tran <quantl@caro.vn>
2019-12-05 00:24:28 +0700

    Init event view

---
+++ ios/Modules/Event/Cell/EventCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  EventCollectionViewCell.swift
+//  Cupid
+//
+//  Created by Quan Tran on 12/4/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
