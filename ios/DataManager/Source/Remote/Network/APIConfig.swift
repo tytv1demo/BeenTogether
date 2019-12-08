a596d4c2c67b507870054fe33cea1db560927ecc
ty tran <tytv1@fsoft.com>
2019-12-08 16:33:27 +0700

    add react native support

--- ios/DataManager/Source/Remote/Network/APIConfig.swift
+++ ios/DataManager/Source/Remote/Network/APIConfig.swift
@@ -73,7 +73,7 @@ extension String {
 
 enum APIEndpoint: String {
     case baseURL = "https://cupid-api.tranty9597.now.sh"
-//     case baseURL = "http://172.16.1.14:3000"
+//     case baseURL = "http://localhost:3000"
 }
 
 enum JSONKey: String {
