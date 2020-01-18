862a0b86f82ac885d81e1a0f80a8d04d92085ce0
DungNQ16 <dungnq16@fsoft.com.vn>
2020-01-18 15:28:10 +0700

    Add PhoneNumberKit

---
+++ ios/Common/Functions/ParsePhoneNumber.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNumeber.swift
+//  Cupid
+//
+//  Created by Dung Nguyen on 1/18/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+import PhoneNumberKit
+
+func parsePhoneNumber(_ phoneNumber: String, _ regionCode: String) throws -> String {
+    let phoneNumberKit = PhoneNumberKit()
+    let parsedPhoneNumber = try phoneNumberKit.parse(phoneNumber, withRegion: regionCode, ignoreType: true)
+    
+    return parsedPhoneNumber.numberString
+}
